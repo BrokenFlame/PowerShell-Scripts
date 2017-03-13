12dfa15ad5719800226be2e3db28ea2629b7658b
Tr <tr.thambi@gmail.com>
2017-03-13 13:15:58 +0000

    Update AddLoginToServerRole.sql

--- SQL/AddLoginToServerRole.sql
+++ SQL/AddLoginToServerRole.sql
@@ -1,3 +1,4 @@
+[void][System.Reflection.Assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo")
 $sqlSrv = New-Object 'Microsoft.SqlServer.Management.Smo.Server' "(local)"
 	
 $login = New-Object -TypeName Microsoft.SqlServer.Management.Smo.Login -ArgumentList $sqlSrv, "DOMAIN\username"
